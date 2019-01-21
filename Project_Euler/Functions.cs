8382b91904b581d6b0a320d0484938228aeed0d8
Patrick <patrick.the.cs.engineer@gmail.com>
2019-01-21 21:43:58 +0200

    Problem 2 complete
    Problem 1

---
+++ Project_Euler/Functions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Euler
+{
+    public class Functions
+    {
+
+        public  bool IsPrime(int value)
+        {
+            for (int i = 2; i < value / 2; i++)
+            {
+                if (value % i == 0)
+                    return true;
+            }
+            return false;
+        }
+
+        public  int fib(int num1)
+        {
+            if (num1 == 1)
+                return 1;
+
+            if (num1 == 2)
+                return 2;
+            else
+
+                //Console.WriteLine(num1);
+                return fib(num1 - 1) + fib(num1 - 2);
+        }
+    }
+}
---
+++ Project_Euler/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Euler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //problem2();
+
+            problem3();
+            Console.ReadKey();
+        }
+
+        private void problem1()
+        {
+            int max = 1000;
+            int sum = 0;
+            for (int i = 0; i < max; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    sum += i;
+                    continue;
+                }
+
+                if (i % 5 == 0)
+                {
+                    sum += i;
+                    continue;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+
+        private static void problem2()
+        {
+            Functions myFunction = new Functions();
+            int max = 40;
+            int sum = 0;
+            int value = 0;
+
+            for (int i = 1; i <= max; i++)
+            {
+                value = myFunction.fib(i);
+                if (value > 4000000)
+                    break;
+
+                if (value % 2 == 0)
+                {
+                    sum += value;
+                    
+                }
+
+                Console.Write($"{value}, ");
+            }
+
+            Console.WriteLine("Final sum is: " + sum);
+        }
+
+        private static void problem3()
+        {
+            Functions functions = new Functions();
+
+            Console.WriteLine(
+                functions.IsPrime(13)
+                    ? "prime"
+                    : "Not Prime");
+                
+
+        }
+
+        
+
+
+    }    
+}
